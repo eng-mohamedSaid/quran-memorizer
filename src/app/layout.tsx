6dc0511062772a86c2823c0e1131f8811093bdc3
Mohamed Said <ms277712@gmail.com>
2025-08-25 15:56:32 +0000

    edit title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'صاحب القرآن - Quran Companion',
-  description: 'تجربة فريدة ومُلهمة لحفظ ومراجعة القرآن الكريم عبر الاستماع.',
+  title: 'صاحب القرآن',
+  description: 'تجربة فريدة ومُلهمة لحفظ ومراجعة القرآن الكريم عبر تكرار السماع للايات وحفظها من خلال تكرار الاية بصوت اكثر من شيخ.',
 };
 
 export default function RootLayout({
@@ -18,6 +18,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://img.icons8.com/external-bzzricon-outline-bzzricon-studio/64/external-quran-ramadan-bzzricon-outline-bzzricon-outline-bzzricon-studio.png"
+        />
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         {children}
